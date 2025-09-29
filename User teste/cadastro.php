e8c10cb83e285a19c73ec911f43d682c13ee14db
João Pedro <joaopb859@gmail.com>
2025-09-29 20:57:50 -0300

    Terceiro commit

--- User teste/cadastro.php
+++ User teste/cadastro.php
@@ -7,7 +7,8 @@
     $senha = $_POST['senha'];
     $telefone = $_POST['telefone'];
 
-    //echo "olá tudo bem?"
+    //echo "olá tudo bem?" SIM  isto foi um teste de git
+    //Mais um teste de git
 
     //Comando SQL
     $sql = "INSERT INTO usuario (nome, email, senha, telefone)
